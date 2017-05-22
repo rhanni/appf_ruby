9b12b38db9da285cb900ccb6e8d66b3ed54e9352
Ryan Hanni <ryan.hanni@appfolio.com>
2017-05-22 15:37:36 -0700

    rh/jm finished module 14

--- studio_game/game.rb
+++ studio_game/game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 
 class Game
     attr_accessor :name, :players_array
@@ -8,25 +9,15 @@ class Game
         @name = name
         @players_array = []
     end
-    
+
     def add_player(player)
         @players_array.push(player)
     end
 
     def play
-        die = Die.new()
-        puts "There are #{@players_array.length} in #{@name}"
         @players_array.each do |player|
+            GameTurn.take_turn(player)
             puts player
-            case die.roll
-                when 1..2
-                    player.blam
-                when 3..4
-                    puts "#{player} was skipped."
-                else
-                    player.w00t
-            end
         end
-        
     end
 end
