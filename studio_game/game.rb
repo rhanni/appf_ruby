326f4f547e48f07e93d1c5b38e31c13227fd82f9
Ryan Hanni <ryan.hanni@appfolio.com>
2018-04-23 16:51:00 -0700

    rh - module 19 read players from input file

--- studio_game/game.rb
+++ studio_game/game.rb
@@ -52,4 +52,12 @@ class Game
     def print_player_name_and_health(player)
         puts  "Name: #{player.name}, Health: #{player.health}"
     end
+
+    def load_players(from_file)
+        File.readlines(from_file).each do |line|
+            name, health = line.split(",")
+            player = Player.new(name, Integer(health))
+            add_player(player)
+        end
+    end
 end
--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -5,9 +5,7 @@ player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
 knuckleheads = Game.new("Knuckleheads")
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
-knuckleheads.add_player(player3)
+knuckleheads.load_players(ARGV.shift || "players.csv")
 
 loop do
     puts "\n How many game rounds? (type 'quit' to exit)"
@@ -15,7 +13,7 @@ loop do
     case answer
     when /^\d+$/
         knuckleheads.play(answer.to_i)
-    when 'quit', 'exit'
+    when 'quit', 'exit', 'q'
         knuckleheads.print_stats
         break
     else
