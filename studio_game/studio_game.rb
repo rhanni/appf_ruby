0854d615e906b1bf3653acdd60f281c8d19e6ee8
Ryan Hanni <ryan.hanni@appfolio.com>
2018-04-23 16:18:47 -0700

    rh module 19, added basic i/o for number of game rounds

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -8,5 +8,17 @@ knuckleheads = Game.new("Knuckleheads")
 knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
-knuckleheads.play(2)
-knuckleheads.print_stats
+
+loop do
+    puts "\n How many game rounds? (type 'quit' to exit)"
+    answer = gets.chomp.downcase
+    case answer
+    when /^\d+$/
+        knuckleheads.play(answer.to_i)
+    when 'quit', 'exit'
+        knuckleheads.print_stats
+        break
+    else
+        puts "Enter a number of rounds or type 'quit'"
+    end
+end
